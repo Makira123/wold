f51090214fc09f7bb640c8695f8a8a52d147d37a
Makira123 <167422086+Makira123@users.noreply.github.com>
2026-04-15 04:39:14 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/discordwebp.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(PORT, () => {

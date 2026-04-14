29570f8bd677f699947fcc6b8a3e012907bed1fe
Makira123 <167422086+Makira123@users.noreply.github.com>
2026-04-15 03:48:57 +0900

    Include bot.js in server setup
    Added bot.js module to server initialization.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+require("./bot.js");
+
 // ให้ใช้ไฟล์ในโฟลเดอร์เดียวกัน
 app.use(express.static(__dirname));
 

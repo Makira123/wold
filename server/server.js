b69705aa2aa43f12b928b0fe494b8dea934eb575
Makira123 <167422086+Makira123@users.noreply.github.com>
2026-04-15 04:48:04 +0900

    Refactor server.js for improved readability

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-require("./bot.js");
-
 // ให้ใช้ไฟล์ในโฟลเดอร์เดียวกัน
 app.use(express.static(__dirname));
 

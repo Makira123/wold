7b68ee0d89b951d5deaf2ac07237cda512938609
Makira123 <167422086+Makira123@users.noreply.github.com>
2026-04-16 00:12:11 +0900

    Refactor server.js for improved readability

--- server/server.js
+++ server/server.js
@@ -56,6 +56,6 @@ app.get("/messages/:channelId", async (req, res) => {
   res.json(messages);
 });
 
-app.listen(PORT, "0.0.0.0" () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });

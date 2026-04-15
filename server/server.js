ed921db129ec6de4173fac10238a36dfd1dc8463
Makira123 <167422086+Makira123@users.noreply.github.com>
2026-04-15 18:39:02 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // 🔥 เปิดไฟล์เว็บ (สำคัญ)
-app.use(express.static("public"));
+const path = require("path")
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 // 📦 เก็บข้อมูล
 let data = {};
